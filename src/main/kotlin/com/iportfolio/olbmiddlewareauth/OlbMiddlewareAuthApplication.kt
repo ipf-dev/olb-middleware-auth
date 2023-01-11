b908cecfe891193e557b6078aa3482adb06f50aa
ipf-yjkang <yjkang@iportfolio.co.kr>
2023-01-11 13:49:51 +0900

    Implement [IPFOLB-440] Initial commit

---
+++ src/main/kotlin/com/iportfolio/olbmiddlewareauth/OlbMiddlewareAuthApplication.kt
@@ -0,0 +1,11 @@
+package com.iportfolio.olbmiddlewareauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OlbMiddlewareAuthApplication
+
+fun main(args: Array<String>) {
+	runApplication<OlbMiddlewareAuthApplication>(*args)
+}
---
+++ src/test/kotlin/com/iportfolio/olbmiddlewareauth/OlbMiddlewareAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.iportfolio.olbmiddlewareauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OlbMiddlewareAuthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
