f852d305c36ac19e229216ae6e15fb656a544e89
ipf-yjkang <yjkang@iportfolio.co.kr>
2023-01-20 17:10:32 +0900

    Implement [IPFOLB-498] Redis Configuration Dependencies for Test Code

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,11 +21,16 @@ repositories {
 }
 
 dependencies {
+	// default
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+    testImplementation("junit:junit:4.13")
+
     // redis
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 }
